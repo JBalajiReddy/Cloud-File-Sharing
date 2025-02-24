f94ff3ce0457b01bffc09b0df3749197feeb6767
JBalajiReddy <jeedipallybalajireddy@gmail.com>
2025-02-24 23:08:26 +0530

    Update UserRepository

--- cloud-file-sharing/src/main/java/com/cloud/filesystem/repository/UserRepository.java
+++ cloud-file-sharing/src/main/java/com/cloud/filesystem/repository/UserRepository.java
@@ -1,11 +1,21 @@
 package com.cloud.filesystem.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.cloud.filesystem.entity.User;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    // Query to find user by username
+    @Query("Select u from User u where u.username = :username")
+    User findByUsername(@Param("username") String username);
+
+    //Query to find user by email
+    @Query("Select u from User u where u.email = :email")
+    User findByEmail(@Param("email") String email);
 
 }
