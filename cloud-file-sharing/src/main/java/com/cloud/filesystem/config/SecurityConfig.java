ed020a091ce9851bc3569dde6d8c841d76b40d78
JBalajiReddy <jeedipallybalajireddy@gmail.com>
2025-02-25 22:52:28 +0530

    update SecurityConfig

--- cloud-file-sharing/src/main/java/com/cloud/filesystem/config/SecurityConfig.java
+++ cloud-file-sharing/src/main/java/com/cloud/filesystem/config/SecurityConfig.java
@@ -3,6 +3,6 @@ package com.cloud.filesystem.config;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class SecurityConfig {
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
     // Security configurations will go here
 }
