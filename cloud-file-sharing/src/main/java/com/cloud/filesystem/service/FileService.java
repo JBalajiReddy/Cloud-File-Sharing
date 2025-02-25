61862320d2be6b674c9c6d0be5df195f81f63d0f
JBalajiReddy <jeedipallybalajireddy@gmail.com>
2025-02-25 23:08:35 +0530

    inialize FileDTO

--- cloud-file-sharing/src/main/java/com/cloud/filesystem/service/FileService.java
+++ cloud-file-sharing/src/main/java/com/cloud/filesystem/service/FileService.java
@@ -1,5 +1,7 @@
 package com.cloud.filesystem.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -7,13 +9,64 @@ import com.cloud.filesystem.entity.File;
 import com.cloud.filesystem.repository.FileRepository;
 
 @Service
-public class FileService {
-     @Autowired
-     private FileRepository fileRepository;
+public class FileService extends BaseService<File> {
 
-     public File saveFile(File file) {
-        return fileRepository.save(file);
-     }
+    @Autowired
+    private FileRepository fileRepository;
 
-     
+    @Autowired
+    private UserService userService;
+
+    @Override
+    protected JpaRepository<File, Long> getRepository() {
+        return fileRepository;
+    }
+
+    @Override
+    protected String getEntityName() {
+        return "File";
+    }
+
+    public File uploadFile(FileDTO fileDTO) {
+        File file = new File();
+        file.setName(fileDTO.getName());
+        file.setContentType(fileDTO.getContentType());
+        file.setSize(fileDTO.getSize());
+        file.setOwner(userService.getCurrentUser());
+        file.setPermissions(FilePermission.PRIVATE);
+        return save(file);
+    }
+
+    @PreAuthorize("hasRole('USER') or hasPermission(#id, 'FILE')")
+    public File downloadFile(Long id) {
+        File file = findById(id);
+        if (file.getOwner().getId().equals(userService.getCurrentUser().getId())) {
+            return file;
+        }
+        if (file.getPermissions().equals(FilePermission.PUBLIC)) {
+            return file;
+        }
+        if (file.getSharedWith().contains(userService.getCurrentUser())) {
+            return file;
+        }
+        throw new AccessDeniedException("You don't have permission to access this file");
+    }
+
+    public void shareFile(Long fileId, List<User> users) {
+        File file = findById(fileId);
+        if (!file.getOwner().getId().equals(userService.getCurrentUser().getId())) {
+            throw new AccessDeniedException("You don't own this file");
+        }
+        file.setSharedWith(users);
+        save(file);
+    }
+
+    @PreAuthorize("hasPermission(#id, 'FILE')")
+    public void deleteFile(Long id) {
+        File file = findById(id);
+        if (!file.getOwner().getId().equals(userService.getCurrentUser().getId())) {
+            throw new AccessDeniedException("You don't own this file");
+        }
+        fileRepository.delete(file);
+    }
 }
